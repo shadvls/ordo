a47fce51d59e229d8aa752354a451348dca2106e
shadvls <yanshaaa28@gmail.com>
2013-04-18 12:00:00 +0000

    feat: add TaskDetail view with full edit form
    Implement detail view with form for editing title, description,
    category, priority, and status. Includes Handlebars ifEq helper.

---
+++ static/js/app/views/task-detail.js
@@ -0,0 +1,60 @@
+;(function () {
+
+  Handlebars.registerHelper('ifEq', function (a, b, opts) {
+    return a === b ? opts.fn(this) : opts.inverse(this);
+  });
+
+  App.Views.TaskDetail = Backbone.View.extend({
+
+    el: '#app',
+
+    template: Handlebars.compile($('#tpl-task-detail').html()),
+
+    events: {
+      'submit #form-task-detail': 'save',
+      'click .task-delete': 'deleteTask'
+    },
+
+    initialize: function () {
+      this.listenTo(this.model, 'sync', this.render);
+      this.listenTo(this.model, 'change', this.render);
+      if (this.model.isNew()) {
+        this.model.fetch();
+      }
+    },
+
+    render: function () {
+      this.$el.html(this.template(this.model.toJSON()));
+      return this;
+    },
+
+    save: function (e) {
+      e.preventDefault();
+      var $form = this.$('#form-task-detail');
+      this.model.save({
+        title: $form.find('[name="title"]').val().trim(),
+        description: $form.find('[name="description"]').val().trim(),
+        category: $form.find('[name="category"]').val(),
+        priority: parseInt($form.find('[name="priority"]').val(), 10),
+        status: $form.find('[name="status"]').is(':checked') ? 'done' : 'pending'
+      }, {
+        wait: true,
+        success: function () {
+          Backbone.history.navigate('', { trigger: true });
+        }
+      });
+    },
+
+    deleteTask: function () {
+      if (confirm('Delete this task?')) {
+        this.model.destroy({
+          wait: true,
+          success: function () {
+            Backbone.history.navigate('', { trigger: true });
+          }
+        });
+      }
+    }
+  });
+
+})();
